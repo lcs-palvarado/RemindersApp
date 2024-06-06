784f09516132c7991356b39f5f903f56bba6b4c2
Pablo Alvarado <palvarado@lcs.on.ca>
2024-06-06 11:07:57 -0400

    Initial Commit

---
+++ RemindersApp/RemindersAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemindersAppApp.swift
+//  RemindersApp
+//
+//  Created by Pablo Alvarado on 06/06/24.
+//
+
+import SwiftUI
+
+@main
+struct RemindersAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
